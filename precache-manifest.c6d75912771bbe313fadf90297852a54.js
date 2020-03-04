985ccdbb145a01c6e3e406997664ccc5365d9b46
Yervand Stepanyan <yervand.stepanyan@gmail.com>
2020-03-04 21:23:16 +0400

    Updates

---
+++ precache-manifest.c6d75912771bbe313fadf90297852a54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b7a420592dbe68a512a25fafd233619",
+    "url": "/blog-context-api/index.html"
+  },
+  {
+    "revision": "9344d6f33c3adfbb78df",
+    "url": "/blog-context-api/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "e95a816896db1ff65cfe",
+    "url": "/blog-context-api/static/js/2.6cdee395.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/blog-context-api/static/js/2.6cdee395.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9344d6f33c3adfbb78df",
+    "url": "/blog-context-api/static/js/main.895de8a7.chunk.js"
+  },
+  {
+    "revision": "e489a75b532d3cb7826b",
+    "url": "/blog-context-api/static/js/runtime-main.d0947ee9.js"
+  }
+]);
